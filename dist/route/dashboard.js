3391b799630fd9aa4697e8385a52366dd5f0fd66
cfsp4rk2 <matt.graham@clearfin.io>
2021-05-12 16:33:47 +0100

    dashboard changes

--- dist/route/dashboard.js
+++ dist/route/dashboard.js
@@ -1,7 +1,6 @@
 import ExpressRouterServer from '../lib/ExpressRouterTemp/ExpressRouter.js';
-import User from '../class/User/User.js';
 const router = ExpressRouterServer.route('get', [
-    User.authorise
+// User.authorise
 ], async (request, response) => {
     return response.render('dashboard/dashboard');
 });
